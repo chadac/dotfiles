4a6e46d3771ef02223ed3a04594277400a040263
Chad Crawford <chad@cacrawford.org>
2017-10-05 11:05:08 -0500

    Fixing xmonad install script.
    It was installing old, incorrect packages and did not include packages for trayer and so on.

--- .dotfiles/install/xmonad.sh
+++ .dotfiles/install/xmonad.sh
@@ -3,13 +3,14 @@
 
 ## Should install Xfce4 if necessary
 echo "Installing Xfce4..."
-sudo apt-get install -y xfce4
+sudo apt-get install -y xfce4 xfce4-terminal
 
 echo "Installing Xmonad..."
-sudo apt-get install -y xmonad xmonad-contrib
+sudo apt-get install -y xmonad
 
 ## TODO: Maybe switch to taffybar? https://github.com/travitch/taffybar
-echo "Installing Xmobar..."
-sudo apt-get install -y xmobar
+echo "Installing additional tools..."
+sudo apt-get install -y xmobar trayer suckless-tools
+
 
 echo "Done! To use Xmonad, you will need to log out and log back in with Xfce4."
