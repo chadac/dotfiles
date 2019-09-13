3d0ea80774c6032f4a53dd4a828c58aa35b415df
Chad Crawford <chad@cacrawford.org>
2019-09-12 21:41:50 -0500

    Updated dotfiles with temp repo.

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,4 +1,4 @@
-git clone --bare https://github.com/chadac/dotfiles.git $HOME/.cfg
+git clone --single-branch --branch arch --bare https://github.com/chadac/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
