3350824c38a8b064dc914ad5a451a3ab5164af46
Chad Crawford <chad@cacrawford.org>
2019-09-13 02:56:28 +0000

    Added multi-line config backup to installation script.

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -8,7 +8,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .config-backup/$(dirname {}); mv {} .config-backup/{};'
 fi;
 config checkout
 config config status.showUntrackedFiles no
