07c9add6058f0dd390982c2f455858db75676542
Chad Crawford <charles.a.crawford@exxonmobil.com>
2019-09-17 16:06:57 +0000

    Updated install.sh with correct command. Still seemed to be crashing when installing from scratch.

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -5,11 +5,12 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
+config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .config-backup/$(dirname {}); mv {} .config-backup/{};'
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs --verbose -I{} sh -c 'mkdir -p .config-backup/$(dirname {}); mv {} .config-backup/{};'
 fi;
 config checkout
 config config status.showUntrackedFiles no
