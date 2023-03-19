40f7675bd3ba096cad069bffc7290b3b07aebd36
Chad Crawford <chad@cacrawford.org>
2023-03-19 00:39:58 +0000

    Fix display linking issues, remove commented old code.

--- apps/desktop/xserver/default.nix
+++ apps/desktop/xserver/default.nix
@@ -17,14 +17,18 @@ mkApp {
           + (if(hasAttr "mode" display) then " --mode ${display.mode}" else "")
           + (if(hasAttr "pos" display) then " --pos ${display.pos}" else "")
           + (if(hasAttr "rotate" display) then " --rotate ${display.rotate}" else "")
+          + (if(hasAttr "primary" display && display.primary) then " --primary" else "")
         ) displays)
       );
     in {
       services.xserver = {
         enable = true;
+        # xrandrHeads is another option, but it's too opinionated when your
+        # setup is not chaining desktops left-to-right. Can't change that
+        # without an upstream PR, so the temporary workaround is a separate call
+        # to xrandr right after starting the xserver.
         displayManager.setupCommands =
           lib.mkIf (hasAttr "displays" host) (mkXrandrCmd host.displays);
-            # let output = (mkXrandrCmd host.displays); in builtins.trace output output);
       };
     };
 }
--- hosts/odin/nixos/configuration.nix
+++ hosts/odin/nixos/configuration.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hardware-configuration.nix
     ./virt.nix
+    ./display.nix
 
     # KVM Virtual Machine Configurations
     ./vms/workspace.nix
