a909ccef2c8d9503d066f202f41d89cac2716592
Chad Crawford <chadcr@amazon.com>
2023-03-28 06:11:34 -0700

    Add baldur host

---
+++ hosts/baldur/default.nix
@@ -0,0 +1,8 @@
+{
+  type = "home-manager";
+  system = "x86_64-linux";
+  username = "chadcr";
+  homeDirectory = "/home/ANT.AMAZON.COM/chadcr";
+  email = "chadcr@amazon.com";
+  getApps = apps: [ apps.main ];
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -2,6 +2,7 @@ let
   hosts = {
     odin = import ./odin;
     forseti = import ./forseti;
+    baldur = import ./baldur;
   };
   genHostConfig = hostname: config: config // { inherit hostname; };
 in builtins.mapAttrs genHostConfig hosts
