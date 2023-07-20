98ddcc58498d9394f006309b0aead6fac6da571b
Chad Crawford <chad@cacrawford.org>
2023-07-19 21:29:53 -0400

    feat: add lxd

--- apps/virt/default.nix
+++ apps/virt/default.nix
@@ -1,5 +1,16 @@
-{ call, homePackage }:
+{ call, mkApp, homePackage }:
 {
   libvirtd = call ./libvirtd { };
   virt-manager = homePackage ./. "virt-manager";
+  lxd = mkApp {
+    src = ./.;
+    nixos = { ... }: {
+      virtualisation.lxc = {
+        systemConfig = ''
+          lxc.cgroup2.devices.allow: c 189:* rwm
+        '';
+      };
+      virtualisation.lxd.enable = true;
+    };
+  };
 }
