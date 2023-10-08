6b736e9d113a296b2e06129be7519d39f77099a8
Chad Crawford <chad@cacrawford.org>
2023-10-08 10:12:35 -0400

    feat(term): add essential utilities for terminal usage

--- apps/terminal/default.nix
+++ apps/terminal/default.nix
@@ -1,5 +1,18 @@
-{ call }:
+{ call, mkApp }:
 {
   git = call ./git { };
   zsh = call ./zsh { };
+  essential = mkApp {
+    src = ./.;
+    home = { pkgs, ... }: {
+      home.packages = with pkgs; [
+        unzip
+        hyperfine
+        dig
+        jq
+        lm_sensors
+        pciutils
+      ];
+    };
+  };
 }
