261628c1eb118cc3940a948a61b562054aef5f32
Chad Crawford <chad@cacrawford.org>
2024-04-26 16:15:47 -0400

    fix: use kitty version as specified in home-manager
    * useful so that I can stay consistent and use the version of kitty
      used elsewhere on my system

--- apps/display/i3/home.nix
+++ apps/display/i3/home.nix
@@ -43,7 +43,7 @@ in {
 
       keybindings = {
         "${mod}+d" = "exec '${pkgs.dmenu}/bin/dmenu_run'";
-        "${mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+        "${mod}+Return" = "exec ${lib.getExe config.programs.kitty.package}";
         "${mod}+Shift+q" = "kill";
         "${mod}+Shift+t" = "exec '${pkgs.i3lock}/bin/i3lock -p'";
         "${mod}+Shift+e" = ''exec "${i3-nagbar} -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'"'';
