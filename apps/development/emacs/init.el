856f215f2f0a261ba624851ffb7548ed536c9ad4
Chad Crawford <chad@cacrawford.org>
2024-04-29 07:28:13 -0400

    feat(apps/emacs): add multiple-cursors

--- apps/development/emacs/init.el
+++ apps/development/emacs/init.el
@@ -55,6 +55,12 @@
 ;;   :config
 ;;   (add-to-list 'company-backends 'company-jedi))
 
+;; multiple-cursors
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines))
+
 ;; PROJECTILE
 (use-package projectile
   :ensure t
