15edb749762efbcb705ff132e51f83440a8bf086
Chad Crawford <chad@cacrawford.org>
2025-06-10 12:38:40 -0400

    enable nix-direnv by default
    * faster direnv activation

--- apps/development/default.nix
+++ apps/development/default.nix
@@ -62,7 +62,10 @@ in {
     direnv = {
       inherit tags;
       home = {
-        programs.direnv.enable = true;
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
         programs.git.ignores = [
           ".envrc"
           ".direnv/"
